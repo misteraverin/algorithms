f65d6e60ff4e87f5220e1cc3677fe079e780795d
Max <amax0703@gmail.com>
2016-08-30 19:38:53 +0300

    new standard algos

---
+++ merge_sort_invasions.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+#include <set>
+#include <time.h>
+
+#define TASK_NAME "qsort"
+#define MAXN int(1e6) + 10
+                             
+using namespace std;
+int n, z, k, a[MAXN], b[MAXN], sum_invers = 0;
+
+
+void merge(int low, int high){        
+        int mid = (low + high) / 2;
+        int i = 0;
+        int pos1 = low;      
+        int pos2 = mid + 1;                  
+        while((pos1 <= mid)&&(pos2 <= high)){
+          if(a[pos1] <= a[pos2]){
+                    sum_invers += pos2 - mid - 1;
+                    b[i] = a[pos1];
+                    pos1++;
+                }
+                else{
+                    b[i] = a[pos2];
+                    pos2++;                    
+                }
+          i++;
+        }
+        
+        if(pos1 > mid){
+            for(int j = pos2;j <= high; j++){
+                b[i] = a[j];           
+                i++;         
+            }
+        }
+        else{
+            for(int j = pos1; j <= mid; j++){
+                b[i] = a[j];               
+                sum_invers += pos2 - mid - 1;           
+                i++;
+            }
+        }
+      
+        for(int j = 0;j <= high - low; j++)
+              a[j + low] = b[j];             
+    
+}
+
+                   
+void mergesort(int l, int r){
+  int mid;  
+  if(l < r){
+    mid = (l + r) / 2;
+    mergesort(l, mid);
+    mergesort(mid + 1, r);
+    merge(l, r); 
+  }
+}
+
+
+int main()
+{
+    freopen(TASK_NAME".in", "r", stdin);
+    freopen(TASK_NAME".out", "w", stdout);
+    srand(time(0));
+    while(1){
+        int sum = 0;
+        n = rand() % 10;
+        for(int i = 0; i < n; i++)
+            a[i] = rand() % 1000;
+        for(int i = 0; i < n; i++)
+          for(int j = i + 1; j < n; j++)
+            if(a[i] > a[j])
+              sum++;
+        fprintf(stderr, "%d%d%d-> ", z);
+        for(int i = 0; i < n; i++)
+          fprintf(stderr, "%d ",a[i]);
+        fprintf(stderr,"\n");                          
+        mergesort(0, n - 1);
+        if(sum == sum_invers)
+           fprintf(stderr, "TEST\n");
+        else{
+           fprintf(stderr, "Fail %d %d\n", sum, sum_invers);
+        break;
+        }
+        sum_invers = 0;
+        z++;
+    }
+    return 0;
+}
---
+++ set_on_hashtable.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+const int N = (int) 1e6 + 33;
+ 
+struct Node {
+    Node* next;
+    Node* prev;
+    int value;
+    Node(Node *l, Node *r, int val) {
+        this -> prev = l;
+        this -> next = r;
+        this -> value = val;
+    }
+};
+ 
+Node *h[N];
+ 
+bool check(int a) {
+    int i = a % N;
+    Node *cur = h[i];
+    while (cur) {
+        if (cur -> value == a) return true;
+        cur = cur -> next;
+    }
+    return false;
+}
+ 
+ 
+void add(int a) {
+    int i = a % N;
+    if (check(a)) return;
+    Node* cur = h[i];
+    while (cur -> next) cur = cur -> next;
+    (Node*&)(cur -> next) = new Node(cur, NULL, a);
+}
+ 
+void cancel(int a) {
+    int i = a % N;
+    Node* cur = h[i];
+    while (cur && cur -> value != a) cur = cur -> next;
+    if (cur) {
+        if (cur -> prev) (Node *&)cur -> prev -> next = cur -> next;
+        if (cur -> next) (Node *&)cur -> next -> prev = cur -> prev;
+        (Node *&)cur = NULL;
+    }
+}
+ 
+int main() {
+    char s[30];
+    int a;
+    for (int i = 0; i < N; ++i) {
+        h[i] = new Node(NULL, NULL, -1);
+    }
+    while (scanf("%s %d\n", s, &a) == 2) {
+        a += (int) 1e9;
+        if (s[0] == 'i') {
+            add(a);
+        } else if (s[0] == 'e') {
+            if (check(a)) puts("true");
+            else puts("false");
+        } else {
+            cancel(a);
+        }
+    }
+    return 0;
+}
